d38a01d5490af89a6943d1ccf54ca0104de22f79
dyestuff <axtemramazanov@mail.ru>
2026-05-17 01:19:48 +0300

    feat: add dashboard routes, update layout & fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,1 +1,8 @@
-git
+import { Inter } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({
+  weight: ['400','700'],
+  subsets: ['latin']
+})
