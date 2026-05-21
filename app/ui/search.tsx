5e6b5ea0e4ad901322bce201280bb13f56ebcad4
dyestuff <axtemramazanov@mail.ru>
2026-05-21 19:18:58 +0300

    fead: added search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,23 +1,27 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import {useRouter, usePathname, useSearchParams} from "next/navigation";
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname()
+  const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
+
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+
     replace(`${pathname}?${params.toString()}`);
-    console.log(term);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
