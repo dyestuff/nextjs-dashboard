a2c715236ca7d8dd74b6d91653c7dce85945772f
dyestuff <axtemramazanov@mail.ru>
2026-05-24 23:27:25 +0300

    feat: adding metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,11 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import {Suspense} from "react";
 import {SearchParams} from "next/dist/server/request/search-params";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Invoices'
+}
 
 export default async function Page(props: {
   searchParams?: Promise<{
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import '@/app/ui/global.css'
 import {inter} from "@/app/ui/fonts";
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Dashboard App',
+}
 
 export default function RootLayout({
   children,
